e09bb48acf8e6582d57c129536e402cacdd5dbc9
allamymp <allamympereira@gmail.com>
2023-05-03 06:44:01 -0300

    code updated and first Query

--- jdbc2/src/application/Program.java
+++ jdbc2/src/application/Program.java
@@ -1,9 +1,37 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import db.DB;
+
 public class Program {
-    public static void main(String[] args){
-        Connection connection = DB.getConnection();
+
+    public static void main(String[] args) {
+
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+           Connection connection = DB.getConnection();
+
+            statement = connection.createStatement();
+
+            resultSet = statement.executeQuery("select * from department");
+
+            System.out.println("Id   Name");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeResultSet(resultSet);
+            DB.closeStatement(statement);
+            DB.closeConnection();
+        }
     }
 }
