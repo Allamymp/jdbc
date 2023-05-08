dd0ab62bd389462b9dc89ce0874d2baa3f77444f
allamymp <allamympereira@gmail.com>
2023-05-08 14:37:53 -0300

    jdbc3 updated

--- jdbc3/src/application/Program.java
+++ jdbc3/src/application/Program.java
@@ -2,9 +2,7 @@ package application;
 
 import db.DB;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -20,7 +18,8 @@ public class Program {
                     "INSERT INTO seller "
                     + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
                     +"VALUES "
-                    +" (?, ?, ?, ?, ?)");
+                    +" (?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
             st.setString(1,"Carl Purple");
             st.setString(2,"carl@gmail.com");
             st.setDate(3,new java.sql.Date(sdf.parse("22/04/1985").getTime()));
@@ -28,8 +27,17 @@ public class Program {
             st.setInt(5,4);
 
             int rowsAffected = st.executeUpdate();
-            System.out.println("Rows affected: " + rowsAffected);
-
+            //System.out.println("Rows affected: " + rowsAffected);
+            if(rowsAffected > 0){
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()){
+                    int id = rs.getInt(1);
+                    System.out.println("Executed! Id= "+id);
+                }
+            }
+            else{
+                System.out.println("No rows affected");
+            }
         }
         catch (SQLException | ParseException e){
             e.printStackTrace();
