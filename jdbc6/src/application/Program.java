b240190d68801247574c31fcc67101014af082cb
allamymp <allamympereira@gmail.com>
2023-05-12 03:22:35 -0300

    jdbc6 updated

--- jdbc6/src/application/Program.java
+++ jdbc6/src/application/Program.java
@@ -8,26 +8,41 @@ import db.DbIntegrityException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Connection conn = null;
-        PreparedStatement st = null;
+
+        Statement st = null;
         try {
             conn = DB.getConnection();
-            st = conn.prepareStatement(
-                "DELETE FROM department "
-                    +"WHERE "
-                    +"Id= ?");
-           // st.setInt(1,5);
-            st.setInt(1,2);
-            int rowsAffected = st.executeUpdate();
-            System.out.println("Done! Rows affected: " + rowsAffected);
+
+            conn.setAutoCommit(false);
+            st = conn.createStatement();
+            int rows1 = st.executeUpdate(
+                    "UPDATE seller SET baseSalary = 1090 WHERE DepartmentId=1");
+        /*
+            int x = 1;
+            if (x < 2) {
+                throw new SQLException("FAKE ERROR");
+            }
+        */
+            int rows2 = st.executeUpdate(
+                    "UPDATE seller SET baseSalary = 2090 WHERE DepartmentId=2");
+            conn.commit();
+            System.out.println("rows1: " + rows1);
+
+            System.out.println("rows2: " + rows2);
         } catch (SQLException e) {
-           // e.printStackTrace();
-            throw new DbIntegrityException(e.getMessage());
+            try {
+                conn.rollback();
+                throw new DbException("Transaction fail! Transaction rolled back! Error: "+e.getMessage());
+            } catch (SQLException f) {{
+                throw new DbException("Error trying to rollback! Caused by: " + f.getMessage());
+            }}
         } finally {
             DB.closeStatement(st);
             DB.closeConnection();
